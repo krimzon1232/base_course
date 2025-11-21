f5709c6a9074c76575726724eae372761f0ae9a5
krimzon1232 <119003596+krimzon1232@users.noreply.github.com>
2025-11-21 14:24:53 +0000

    update

--- lec5.py
+++ lec5.py
@@ -1,10 +1,15 @@
 import numpy as np
+
 N=7
 M=3
+
 ar = np.zeros((N, M+1))
+
 for i in range(N):
     for j in range(M):
         n = np.sin(N*i + M*j)
+        
         if n >= 0:
             ar[i, j] = n
+
 print(ar[::, ::-1])
