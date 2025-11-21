607dfc484d740a6f5839238ee3b3152ebebb9a52
krimzon1232 <119003596+krimzon1232@users.noreply.github.com>
2025-11-21 14:59:03 +0000

    dop1 and more

--- dop1.py
+++ dop1.py
@@ -1,1 +1,41 @@
 import numpy as np
+
+def ms_inp(x, y):
+    ls = []
+    for yd in range(y):
+        yls = []
+        
+        for xd in range(x):
+            inp = int(input(f"введите число {xd} строки {yd}: "))
+            yls.append(inp)
+            
+        ls.append(yls)
+        
+    print("Ввод завершен \n")
+    return(np.array(ls))
+
+def ms_checker(m1, m2, x, y):
+    ls = []
+    for yd in range(y):
+        yls = []
+        
+        for xd in range(x):
+            if m1[yd][xd] >= m2[yd][xd]:
+                i = m1[yd][xd]
+            else:
+                i = m2[yd][xd]
+            yls.append(i)
+        
+        ls.append(yls)
+        
+    return(np.array(ls))
+
+
+xi, yi = map(int, input("введите размер масивов через пробел: ").split())
+
+
+mass1 = ms_inp(xi, yi)
+mass2 = ms_inp(xi, yi)
+mass3 = ms_checker(mass1, mass2, xi, yi)
+
+print(mass3)
